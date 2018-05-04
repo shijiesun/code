e3c7e4c5e40450dcd2bab72a741cb9d6a381260d
ssj <ssj280@gmail.com>
2018-05-04 11:24:52 +0800

    use a[i] to replace *(a + i)

--- cpp/sort/sort.hpp
+++ cpp/sort/sort.hpp
@@ -13,7 +13,7 @@ namespace ssj {
         template<typename T>
         void print(const T * arr, int begin, int end) {
             for (int i = begin; i < end; ++i) {
-                cout << *(arr + i) << " ";
+                cout << arr[i] << " ";
             }
             cout << endl;
         }
@@ -43,8 +43,8 @@ namespace ssj {
 
             for (int i = begin; i < end; ++i) {
                 for(int j = begin; j < end - i - 1; ++j) {
-                    if(less(*(arr + j + 1), *(arr + j))) {
-                        swap(*(arr + j), *(arr + j + 1));
+                    if(less(arr[j + 1], arr[j])) {
+                        swap(arr[j], arr[j + 1]);
                         print(arr, begin, end);
                     }
                 }
@@ -59,12 +59,12 @@ namespace ssj {
 
             for (int i = begin + 1; i < end; ++i) {
                 for(int j = begin; j < i; ++j) {
-                    if(less(*(arr + i), *(arr + j))) {
-                        T a = move(*(arr + i));
+                    if(less(arr[i], arr[j])) {
+                        T a = move(arr[i]);
                         for(int k = i - 1; k >= j; --k) {
-                            *(arr + k + 1) = *(arr + k);
+                            arr[k + 1] = arr[k];
                         }
-                        *(arr + j) = a;
+                        arr[j] = a;
                         print(arr, begin, end);
                     }
                 }
@@ -78,16 +78,16 @@ namespace ssj {
 
             while(i < j) {
 
-                while(i < j && !less(*(arr + j), *(arr + begin))) {
+                while(i < j && !less(arr[j], arr[begin])) {
                     --j;
                 }
 
-                while(i < j && !less(*(arr + begin), *(arr + i))) {
+                while(i < j && !less(arr[begin], arr[i])) {
                     ++i;
                 }
 
-                if(i <= j && less(*(arr + j), *(arr + i))) {
-                    swap(*(arr + i), *(arr + j));
+                if(i <= j && less(arr[j], arr[i])) {
+                    swap(arr[i], arr[j]);
                     cout << "swap_ij:(" << i << "," << j << ")" << endl;
                     print(arr, begin, end);
                 }
@@ -110,8 +110,8 @@ namespace ssj {
 
             cout << "pivot:" << pivot << endl;
 
-            if(less(*(arr + pivot), *(arr + begin))) {
-                swap(*(arr + begin), *(arr + pivot));
+            if(less(arr[pivot], arr[begin])) {
+                swap(arr[begin], arr[pivot]);
                 cout << "swap_pivot:(" << pivot << "," << pivot << ")" << endl;
                 cout << "after:\t";
                 print(arr, begin, end);
@@ -129,11 +129,11 @@ namespace ssj {
             bool flag = false;
             for(int i = end - 1; i > begin; --i) {
                 int parent = (i - 1) >> 1;
-                if(less(*(arr + i), *(arr + parent))) {
-                    swap(*(arr + i), *(arr + parent));
+                if(less(arr[i], arr[parent])) {
+                    swap(arr[i], arr[parent]);
 
                     flag = true;
-                    //cout << "swap(" << *(arr + i) << "," << *(arr + parent) << ")" << endl;
+                    //cout << "swap(" << arr[i] << "," << arr[parent) << ")" << endl;
                     //print(arr, begin, end);
                 }
             }
@@ -148,15 +148,15 @@ namespace ssj {
                 int child2 = child1 - 1;
 
                 int child = -1;
-                if(less(*(arr + child1), *(arr + child2))) {
+                if(less(arr[child1], arr[child2])) {
                     child = child1;
                 } else {
                     child = child2;
                 }
 
-                if(child < end && less(*(arr + child), *(arr + i))) {
-                    swap(*(arr + child), *(arr + i));
-                    cout << "swap(" << *(arr + child) << "," << *(arr + i) << ")" << endl;
+                if(child < end && less(arr[child], arr[i])) {
+                    swap(arr[child], arr[i]);
+                    cout << "swap(" << arr[child] << "," << arr[i] << ")" << endl;
                     print(arr, begin, end);
                 }
                 i = child;
@@ -175,8 +175,8 @@ namespace ssj {
             print(arr, begin, end);
 
             for (int i = begin; i < end; ++i) {
-                swap(*(arr + begin), *(arr + end - i - 1));
-                cout << "pop swap(" << *(arr + begin) << "," << *(arr + end - i - 1) << ")" << endl;
+                swap(arr[begin], arr[end - i - 1]);
+                cout << "pop swap(" << arr[begin] << "," << arr[end - i - 1] << ")" << endl;
                 print(arr, begin, end - i - 1);
 
 
@@ -192,9 +192,9 @@ namespace ssj {
         static void shell_insert(T * arr, int begin, int end, int d) {
 
             for (int i = begin; i < end; ++i) {
-                for(int j = begin; j + d < end - i - 1; ++j) {
-                    if(less(*(arr + j + d), *(arr + j))) {
-                        swap(*(arr + j), *(arr + j + d));
+                for(int j = begin; j + d < end; ++j) {
+                    if(less(arr[j + d], arr[j])) {
+                        swap(arr[j], arr[j + d]);
                         cout << "d=" << d << " swap(" << j << "," << j + d << ")" << endl;
                         print(arr, begin, end);
                     }
@@ -250,7 +250,7 @@ namespace ssj {
             }
 
             for (int i = 0; i < end - begin; ++i) {
-                *(arr + begin + i) = *(temp + i);
+                arr[begin + i] = temp[i];
             }
 
             cout << "after:\t";
@@ -272,6 +272,32 @@ namespace ssj {
 
         }
 
+        template<typename T>
+        int max(T * arr, int begin, int end) {
+            int max = 0;
+            for (int i = begin; i < end; ++i) {
+                if(arr[i] > max) {
+                    max = arr[i];
+                }
+            }
+            return max;
+        }
+
+        template<typename T>
+        int min(T * arr, int begin, int end) {
+            int min = arr[begin];
+            for (int i = begin + 1; i < end; ++i) {
+                if(arr[i] < min) {
+                    min = arr[i];
+                }
+            }
+            return min;
+        }
+
+        template<typename T>
+        void counting_sort(T * arr, int begin, int end) {
+
+        }
     }
 
 
